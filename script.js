1e559398b6b6b9e05e14ad5918f890162b64f6d5
abantoo <abantonaimur@gmail.com>
2021-04-09 19:25:30 +0600

    nothing done yet

---
+++ script.js
@@ -0,0 +1,19 @@
+//first to win 10 points
+
+//from user
+const userInput;
+
+//from random generator
+
+const allOptions = ["rock", "paper", "scissors"];
+
+const aiTurn = allOptions[Math.floor(Math.random() * 3 + 1)];
+
+/*
+
+if !userInput within timer generate a random from rockpaper and SpeechSynthesisErrorEvent,
+else use userInput
+
+*/
+
+Number(userInput) == aiTurn; //draw
